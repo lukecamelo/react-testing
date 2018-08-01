6e783872bb3aabb8be24c498edcb6e748c30dfb1
Luke Camelo <azathothbeats@gmail.com>
2018-07-31 20:52:07 -0400

    Implemented extremely basic test

---
+++ src/components/TestComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class TestComponent extends React.Component {
+  render() {
+    return (
+      <div className="TestComponent">
+        <h1>I'm a test component</h1>
+        <p>Oh yes I am</p>
+      </div>
+    )
+  }
+}
+
+export default TestComponent
---
+++ src/components/TestComponent.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestComponent from './TestComponent'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<TestComponent/>, div)
+})
