57240e0427f56bf31c3e0fdb8374cc0dd63fd7c9
Luke Camelo <azathothbeats@gmail.com>
2018-07-31 21:26:28 -0400

    Added second test for h2 element text

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 class TestComponent extends React.Component {
   render() {
+
+    const firstHalf = 'i am a lazy dog '
+    const secondHalf = 'and i like to jump'
+
     return (
       <div className="TestComponent">
         <h1>I'm a test component</h1>
-        <p>Oh yes I am</p>
+        <h2>{firstHalf + secondHalf}</h2>
       </div>
     )
   }
--- src/components/TestComponent.test.js
+++ src/components/TestComponent.test.js
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme'
 import TestComponent from './TestComponent'
 
 describe('<TestComponent/>', () => {
-  it('renders the test component', () => {
+
+  it('renders a single h1 element', () => {
     const tester = shallow(<TestComponent/>)
     expect(tester.find('h1').length).toEqual(1)
   })
+
+  it('displays the whole sentence', () => {
+    const sentence = shallow(<TestComponent/>)
+    expect(sentence.find('h2').text()).toEqual('i am a lazy dog and i like to jump')
+  })
+
 })
+
