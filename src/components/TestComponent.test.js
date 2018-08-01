97785aa3f274c462cd7536e465f7ad4dd5bf74ff
Luke Camelo <azathothbeats@gmail.com>
2018-07-31 23:18:30 -0400

    Refactored box div test

--- src/components/TestComponent.test.js
+++ src/components/TestComponent.test.js
@@ -3,21 +3,23 @@ import { shallow } from 'enzyme'
 import TestComponent from './TestComponent'
 
 describe('<TestComponent/>', () => {
-
   it('renders a single h1 element', () => {
-    const tester = shallow(<TestComponent/>)
+    const tester = shallow(<TestComponent />)
     expect(tester.find('h1').length).toEqual(1)
   })
 
   it('displays the whole sentence', () => {
-    const sentence = shallow(<TestComponent/>)
-    expect(sentence.find('h2').text()).toEqual('i am a lazy dog and i like to jump')
+    const sentence = shallow(<TestComponent />)
+    expect(sentence.find('h2').text()).toEqual(
+      'i am a lazy dog and i like to jump'
+    )
   })
 
   it('adds a box on click', () => {
-    const container = shallow(<TestComponent/>)
-    container.find('button').simulate('click')
-    expect(container.find('div.box').length).toEqual(1)
+    const container = shallow(<TestComponent />)
+    for (let i = 0; i < 10; i++) {
+      container.find('button').simulate('click')
+      expect(container.find('div.box').length).toEqual(i+1)
+    }
   })
 })
-
