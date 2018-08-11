9fe649415a3b6658133020a440d914ddf5733cd4
Luke Camelo <azathothbeats@gmail.com>
2018-08-10 21:59:18 -0400

    Removed unnecessary code

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -18,17 +18,6 @@ class TestComponent extends React.Component {
     })
   }
 
-  // DOESNT WORK ?????
-  // isPrime = value => {
-  //   for (let i = 2; i < value; i++) {
-  //     if (value % i === 0) {
-  //       this.setState({ primeAnswer: false })
-  //     }
-  //   }
-  //   this.setState({ primeAnswer: true })
-  //   console.log(this.state.primeAnswer)
-  // }
-
   render() {
     const firstHalf = 'i am a lazy dog '
     const secondHalf = 'and i like to jump'
@@ -42,17 +31,6 @@ class TestComponent extends React.Component {
       <div className="TestComponent">
         <h1>I'm a test component</h1>
         <h2>{firstHalf + secondHalf}</h2>
-        <h3>{this.primeAnswer}</h3>
-        
-        {/* <input
-          type="text"
-          onChange={this.changeHandler}
-          value={this.state.primeNum}
-        />
-
-        <button onClick={() => this.isPrime(this.state.primeNum)}>
-          get prime
-        </button> */}
 
         <p onClick={this.createBox}>hey</p>
         <button onClick={() => this.createBox()}>Add box</button>
